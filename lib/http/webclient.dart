f1b65162c156e68a933a25fdca44f4153031148e
Igor Coutinho Ferreira Nobre <igorn@MMB025596CPS.local>
2020-11-19 23:26:06 -0300

    first commit

---
+++ lib/http/webclient.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+import 'interceptors/logging_interceptor.dart';
+
+final Client client = HttpClientWithInterceptor.build(
+  interceptors: [LoggingInterceptor()],
+  requestTimeout: Duration(seconds: 5),
+);
+
+const String baseUrl = 'http://172.17.1.152:8080/transactions';
